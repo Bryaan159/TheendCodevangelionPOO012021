5863d90e5e1320dde06ded71fdeea294d6a8dc61
Bryaan159 <00057720@uca.edu.sv>
2021-03-11 18:07:23 -0600

    Upload a folder with the code

---
+++ example1/example1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            Console.WriteLine("Enter a value for a:");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for b:");
+            b = Int32.Parse(Console.ReadLine());
+            
+            //Use of funtion
+            Console.WriteLine($"Adding {a} + {b} equals {AddNumbers(a, b)}");
+        }
+        //Funtion to evaluate two numbers
+        public static int AddNumbers(int a, int b)
+        {
+            return a+b;
+        }
+    }
+}
